c2824484fe0762a2115a8f67c3e344765e497216
Jessie Fernandez <jessievf@gmail.com>
2019-11-29 21:34:21 -0800

    changed a few lines to the int file

--- working_with_ints.cs
+++ working_with_ints.cs
@@ -3,8 +3,12 @@ namespace c_sharp_basics
 {
     public class working_with_ints
     {
-        public working_with_ints()
+       public static void main(string[] args)
         {
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            
         }
     }
 }
